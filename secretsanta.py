4d339fe112c6048dde0af66010cb3e64461796a7
Prerit Das <preritdas@gmail.com>
2022-08-25 20:53:29 -0400

    Refactoring.

---
+++ secretsanta.py
@@ -0,0 +1,53 @@
+# External imports
+import nexmo
+
+# Local imports
+import random 
+import _keys
+
+
+# Instantiate Nexmo client
+nexmo_client = nexmo.Client(
+    key = _keys.nexmo_api_key,
+    secret = _keys.nexmo_api_secret
+)
+sms = nexmo.Sms(client = nexmo_client)
+
+
+def alert_assignment(person: str, assigned: str):
+    """Alerts a person who they were assigned to."""
+    person = person.title()
+    sms.send_message(
+        {
+            "from": _keys.nexmo_sender,
+            "to": _keys.people[person],
+            "text": f"{person}, you have been assigned to {assigned}."
+        }
+    )
+
+
+def run():
+    """Main execution function."""
+    already_assigned = []
+    for person in _keys.people:
+        # Local list of people
+        people = list(_keys.people.keys())
+
+        # Assign person to another person
+        people.remove(person)
+        assigned = random.choice(people)
+        # Duplicate check
+        while assigned in already_assigned:
+            assigned = random.choice(people)
+        # Store the assignment for future iterations
+        already_assigned.append(assigned)
+
+        # Alert the person who they were assigned to
+        alert_assignment(
+            person = person,
+            assigned = assigned
+        )
+
+
+if __name__ == "__main__":
+    main()
